634ed25d5742090add0a272d8de70e86cf67ce8e
Michail Golubjev <xopkop@gmail.com>
2020-03-06 22:21:52 +0100

    Bump version to 2020

--- AutoCADCommands/Properties/AssemblyInfo.cs
+++ AutoCADCommands/Properties/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AutoCADCommands")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("luanshixia")]
 [assembly: AssemblyProduct("AutoCADCommands")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © luanshixia")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("c6783d73-7c70-455d-a952-6ee574581a94")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2020.0.0.0")]
+[assembly: AssemblyFileVersion("2020.0.0.0")]
